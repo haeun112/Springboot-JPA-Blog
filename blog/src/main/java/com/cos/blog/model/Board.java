fdcc88e5b4a2535d5e547d395e95d74d703ed080
haeun112 <134517534+haeun112@users.noreply.github.com>
2023-06-17 19:46:44 +0900

    Add files via upload

---
+++ blog/src/main/java/com/cos/blog/model/Board.java
@@ -0,0 +1,53 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity // 제일 밑에 배치되는게 좋음
+public class Board {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Column(columnDefinition = "LONGTEXT") //대용량 데이터
+	private String content; //섬머노트 라이브러리 <html>태그가 섞여서 디자인이 됨.
+	
+	@ColumnDefault("0")
+	private int count; // 조회수
+	
+	@ManyToOne(fetch = FetchType.EAGER) //Many=Board, User=One
+	@JoinColumn(name="userId")
+	private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
+	
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER)//mappedBy  연관관계의 주인이 아니다 (난 FK가 아니에요) DB에 칼럼을 만들지 마세요.
+	private List<Reply> reply;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+}
